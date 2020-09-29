087d3bd7b2a2d302a71696cf8d10e88db8be8b51
u8x9 <u8x@eclipso.eu>
2020-09-29 16:58:33 +0000

    修改字符串

--- day01/09byte_rune/main.go
+++ day01/09byte_rune/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // go语言为了处理非ASCII字符，定义了 rune 类型
 
@@ -23,4 +25,10 @@ func main(){
         //fmt.Println(c)
         fmt.Printf("%c\n", c)
     }
+
+    // 字符串修改
+    // 需要转成 []byte 或 []rune 再进行修改
+    ss := []rune(s) // 把字符串强转为rune切片
+    ss[5] = '天'
+    fmt.Println(string(ss)) // 把[]rune 转成字符串
 }
