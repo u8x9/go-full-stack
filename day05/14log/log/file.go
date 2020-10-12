16dadcc65e4ad5e3dc5126126ff54201c8fe8b5e
u8x9 <u8x@eclipso.eu>
2020-10-13 01:38:30 +0800

    日志

--- day05/14log/log/file.go
+++ day05/14log/log/file.go
@@ -5,3 +5,11 @@ import "os"
 func GetFileHandler(filename string)(*os.File, error) {
     return os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 }
+
+func MustGetFileHandler(filename string) *os.File {
+    f, err := GetFileHandler(filename)
+    if err!=nil{
+        panic(err)
+    }
+    return f
+}
