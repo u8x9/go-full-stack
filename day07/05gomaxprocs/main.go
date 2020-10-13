c3788dc80f861dbd887c55bd4d0088c799644950
u8x9 <u8x@eclipso.eu>
2020-10-14 02:39:01 +0800

    GOMAXPROCS

---
+++ day07/05gomaxprocs/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func a() {
+    defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("A: %d\n", i)
+	}
+}
+func b() {
+    defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("B: %d\n", i)
+	}
+}
+func main() {
+    runtime.GOMAXPROCS(1)
+	wg.Add(2)
+	go a()
+    go b()
+    wg.Wait()
+}
